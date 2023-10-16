8211e93f29da101ae35ebd79b2289381291146c3
Christopher Stewart <chris.stewart132@gmail.com>
2023-10-17 12:03:02 +1300

    range_to_font json formatting in read.py, hardcoded map temporarily IMPLEMENTATION TODO

--- read.py
+++ read.py
@@ -14,22 +14,22 @@ while i != file.tell():# keep looping while the file index continues to move
     if "U+" in line:
         categories += line
         category = line
-        t = line.split('U+')
-        start, end = '0x' + t[-2].split()[0], '0x' + t[-1].split()[0]
+        t = line.split("U+")
+        start, end = "0x" + t[-2].split()[0], "0x" + t[-1].split()[0]
     elif "Windows" in line:
-        fonts = line.split(":")[-1].split()
+        fonts = line.split(":")[-1].strip().split(", ")
         font_map[category] = fonts
-        range_to_font[(start, end)] = fonts
+        range_to_font[start+","+end] = fonts
 file.close()
 
-output = open("categories.txt", mode='w')
+output = open("categories.txt", mode="w")
 output.write(categories)
 output.close()
 
-output = open("font_map.txt", mode='w')
+output = open("font_map.txt", mode="w")
 output.write(str(font_map))
 output.close()
 
-output = open("range_to_font.txt", mode='w')
+output = open("range_to_font.txt", mode="w")
 output.write(str(range_to_font))
 output.close()
