9f44c288627cb2e17ea3728398ebd16a58b39c59
Christopher Stewart <chris.stewart132@gmail.com>
2023-10-20 12:30:22 +1300

    read.py generating tools category navigation buttons, implemented category navigation

--- read.py
+++ read.py
@@ -5,6 +5,7 @@ https://www.alanwood.net/unicode/fontsbyrange.html
 
 file = open("fontsbyrange.txt")
 categories = ""
+category_navigation_html = ""
 font_map = {}# category(str)->fonts(list)
 range_to_font = {}# range(int,int)->fonts(list)
 i = -1
@@ -14,6 +15,13 @@ while i != file.tell():# keep looping while the file index continues to move
     if "U+" in line:
         categories += line
         category = line
+        # navigation button html generation
+        try:
+            s, e = category.split("(")[-1].split("-")
+            title = category.split("U+")[0].strip()
+            category_navigation_html += f'<div class="symbolBlock" style="width:226px;" onclick="loadPageRange({s}, {e}"><p class="btn">{title}</p></div>'
+        except:
+            print(category)
         t = line.split("U+")
         start, end = "0x" + t[-2].split()[0], "0x" + t[-1].split()[0]
     elif "Windows" in line:
@@ -34,13 +42,14 @@ output = open("range_to_font.txt", mode="w")
 output.write(str(range_to_font))
 output.close()
 
+output = open("category_navigation_html.txt", mode="w")
+output.write(category_navigation_html)
+output.close()
+
 fonts = {}# map of font_name -> number of ranges compatible with
 for key in range_to_font.keys():
     for font in range_to_font[key]:
         fonts[font] = fonts[font] + 1 if font in fonts else 1
-
 sorted_fonts = sorted(fonts.items(), key=lambda item: item[1], reverse=True)
 top_10_fonts = sorted_fonts[:10]
-print("top 10 fonts:", top_10_fonts)
-        
-#print(range_to_font.keys(), len(range_to_font.keys()))
+#print("top 10 fonts:", top_10_fonts)  
